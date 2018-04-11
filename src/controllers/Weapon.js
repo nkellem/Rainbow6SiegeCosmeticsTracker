5863bbda9bff3bc1cb11c20fb2c809a824c7e57c
Noah Kellem <nmk2485@g.rit.edu>
2018-04-11 08:19:52 -0400

    Added Weapon controller logic

--- src/controllers/Weapon.js
+++ src/controllers/Weapon.js
@@ -7,7 +7,37 @@ const operatorsPage = (req, res) => {
 };
 
 const addWeaponSkin = (req, res) => {
-	
+	const request = req;
+  const response = res;
+
+  if (!request.opName || !request.weaponName || !request.skin) {
+    return response.status(400).json({ error: 'Operator Name, Weapon Name, and a Skin are all required' });
+  }
+
+  const weaponData = {
+    opName: request.body.opName,
+    weaponName: request.body.weaponName,
+    skins: [request.body.skin],
+    owner: request.session.account._id,
+  };
+
+  const newWeapon = new Weapon.WeaponModel(weaponData);
+
+  const weaponPromise = newWeapon.save();
+
+  //add some response here
+
+  weaponPromise.catch(err => {
+    console.log(err);
+    if (err.code === 11000) {
+      //TODO: Add logic to add to existing Weapon
+      return res.status(400).json({ error: 'Weapon already exists' });
+    }
+
+    return res.status(400).json({ error: 'an error ocurred' });
+  });
+
+  return weaponPromise;
 };
 
 module.exports = {
